4ba141c254f6c1bc135fc21fa3702b8f8ab2aaa7
Deepak Kumar <deepak@kreatio.com>
2023-03-31 18:49:01 +0530

    Automated prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 import { allBooks } from "./data/books.js";
 
-function Book({book}) {
+function Book({ book }) {
   return (
     <div className="book" key={book.id}>
       <h3>{book.title} Hello</h3>
-      <img src={`${book.cover}?text=${book.title}`}  alt={book.title}/>
+      <img src={`${book.cover}?text=${book.title}`} alt={book.title} />
       <p>Published in {book.year}</p>
     </div>
-  )
-
+  );
 }
 
 function App() {
-  const [count, setCount] = useState(0);
   const [selectedBooks, setSelectedBooks] = useState(allBooks);
 
-  const bookComponents = selectedBooks.map(book => <Book book={book}></Book>);
+  const bookComponents = selectedBooks.map((book) => <Book book={book}></Book>);
 
   return (
     <div className="App">
       <h1>Learning React</h1>
       {bookComponents}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
