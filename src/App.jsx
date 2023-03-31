0ee23b66e619f5e7fb590c966c10be268eee3aa8
Deepak Kumar <deepak@kreatio.com>
2023-03-31 20:08:01 +0530

    Working version

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,57 @@ function Book({ book }) {
   );
 }
 
+function YearSelector({ year, setYear, setSelectedBooks }) {
+  const onChange = (evt) => {
+    let selectedYear = evt.target.value;
+
+    let books;
+    if (selectedYear === "All") {
+      books = allBooks;
+    } else {
+      selectedYear = parseInt(selectedYear);
+      books = allBooks.filter((book) => book.year === selectedYear);
+    }
+
+    setYear(selectedYear);
+    setSelectedBooks(books);
+    console.log(books.length);
+  };
+
+  const years = Array.from({ length: 12 }, (e, i) => 1959 + i);
+  const options = years.map((year) => <option key={year}>{year}</option>);
+
+  return (
+    <>
+      <label>Year: </label>
+      <select value={year} onChange={onChange}>
+        <option>All</option>
+        {options}
+      </select>
+    </>
+  );
+}
+
 function App() {
   const [selectedBooks, setSelectedBooks] = useState(allBooks);
+  const [year, setYear] = useState("All");
 
   const bookComponents = selectedBooks.map((book) => <Book book={book}></Book>);
 
   return (
     <div className="App">
-      <h1>Learning React</h1>
+      <h1>Books</h1>
+      <div id="control">
+        <p>
+          Showing {selectedBooks.length} of {allBooks.length}
+        </p>
+        <YearSelector
+          year={year}
+          setYear={setYear}
+          setSelectedBooks={setSelectedBooks}
+        />
+      </div>
+
       {bookComponents}
     </div>
   );
